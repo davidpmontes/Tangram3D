0d186d6773d3404fce58e032cda226a5417abdfc
Montes <dpmontes@ndc.nasa.gov>
2020-11-05 00:08:11 -0800

    Tangram3D title

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    private GameObject arch;
+    private GameObject cone;
+    private GameObject cylinder;
+    private GameObject icosphere;
+    private GameObject prism;
+    private GameObject torus;
+
+    public void OnButtonA()
+    {
+
+    }
+
+    public void OnButtonB()
+    {
+
+    }
+
+    public void OnButtonC()
+    {
+
+    }
+}
